db38fee59f728f3b2df3a5c3db35af660a823488
fernandotrevisan <fernando.trevisan01@etec.sp.gov.br>
2023-05-04 14:21:05 -0300

    Adicionar arquivos de projeto.

---
+++ atividade_prova_211494/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace atividade_prova_211494
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblItensGrade = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnNovaVenda = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.txtQuantidadeSelecionada = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQuantidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValorUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.txtValorUnitario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVenda = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblItensGrade
+            // 
+            this.lblItensGrade.AutoSize = true;
+            this.lblItensGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblItensGrade.Location = new System.Drawing.Point(8, 549);
+            this.lblItensGrade.Name = "lblItensGrade";
+            this.lblItensGrade.Size = new System.Drawing.Size(129, 20);
+            this.lblItensGrade.TabIndex = 36;
+            this.lblItensGrade.Text = "Itens na grade: 1";
+            this.lblItensGrade.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTotal.Location = new System.Drawing.Point(13, 514);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(108, 20);
+            this.lblTotal.TabIndex = 35;
+            this.lblTotal.Text = "Total: R$ 0,00";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnFechar.Location = new System.Drawing.Point(284, 610);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(146, 29);
+            this.btnFechar.TabIndex = 34;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancelar.Location = new System.Drawing.Point(284, 575);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(146, 29);
+            this.btnCancelar.TabIndex = 33;
+            this.btnCancelar.Text = "Cancelar Venda";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnNovaVenda
+            // 
+            this.btnNovaVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnNovaVenda.Location = new System.Drawing.Point(132, 610);
+            this.btnNovaVenda.Name = "btnNovaVenda";
+            this.btnNovaVenda.Size = new System.Drawing.Size(146, 29);
+            this.btnNovaVenda.TabIndex = 32;
+            this.btnNovaVenda.Text = "Nova Venda";
+            this.btnNovaVenda.UseVisualStyleBackColor = true;
+            this.btnNovaVenda.Click += new System.EventHandler(this.btnNovaVenda_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnAlterar.Location = new System.Drawing.Point(132, 572);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(146, 29);
+            this.btnAlterar.TabIndex = 31;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // txtQuantidadeSelecionada
+            // 
+            this.txtQuantidadeSelecionada.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.txtQuantidadeSelecionada.Location = new System.Drawing.Point(267, 467);
+            this.txtQuantidadeSelecionada.Name = "txtQuantidadeSelecionada";
+            this.txtQuantidadeSelecionada.Size = new System.Drawing.Size(95, 26);
+            this.txtQuantidadeSelecionada.TabIndex = 30;
+            this.txtQuantidadeSelecionada.Text = "\'";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.label5.Location = new System.Drawing.Point(8, 470);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 20);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Quantidade de Item selecionado:";
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.AllowUserToAddRows = false;
+            this.dgvProdutos.AllowUserToDeleteRows = false;
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDescricao,
+            this.colQuantidade,
+            this.colValorUnitario});
+            this.dgvProdutos.Location = new System.Drawing.Point(12, 168);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.ReadOnly = true;
+            this.dgvProdutos.Size = new System.Drawing.Size(425, 289);
+            this.dgvProdutos.TabIndex = 28;
+            this.dgvProdutos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProdutos_CellContentClick);
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.ReadOnly = true;
+            // 
+            // colQuantidade
+            // 
+            this.colQuantidade.HeaderText = "Quantidade";
+            this.colQuantidade.Name = "colQuantidade";
+            this.colQuantidade.ReadOnly = true;
+            // 
+            // colValorUnitario
+            // 
+            this.colValorUnitario.HeaderText = "Valor Unitário";
+            this.colValorUnitario.Name = "colValorUnitario";
+            this.colValorUnitario.ReadOnly = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(291, 45);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(146, 34);
+            this.btnRemover.TabIndex = 27;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnInserir.Location = new System.Drawing.Point(291, 110);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(146, 34);
+            this.btnInserir.TabIndex = 26;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // txtValorUnitario
+            // 
+            this.txtValorUnitario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.txtValorUnitario.Location = new System.Drawing.Point(122, 121);
+            this.txtValorUnitario.Name = "txtValorUnitario";
+            this.txtValorUnitario.Size = new System.Drawing.Size(129, 26);
+            this.txtValorUnitario.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(8, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 20);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Valor Unitário:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.txtQuantidade.Location = new System.Drawing.Point(113, 80);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(146, 26);
+            this.txtQuantidade.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(8, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Quantidade:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.txtDescricao.Location = new System.Drawing.Point(105, 42);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(154, 26);
+            this.txtDescricao.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(8, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Descrição:";
+            // 
+            // lblVenda
+            // 
+            this.lblVenda.AutoSize = true;
+            this.lblVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblVenda.Location = new System.Drawing.Point(12, 9);
+            this.lblVenda.Name = "lblVenda";
+            this.lblVenda.Size = new System.Drawing.Size(92, 25);
+            this.lblVenda.TabIndex = 19;
+            this.lblVenda.Text = "Venda: 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 642);
+            this.Controls.Add(this.lblItensGrade);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnNovaVenda);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.txtQuantidadeSelecionada);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvProdutos);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtValorUnitario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVenda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblItensGrade;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnNovaVenda;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.TextBox txtQuantidadeSelecionada;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQuantidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValorUnitario;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.TextBox txtValorUnitario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVenda;
+    }
+}
+
---
+++ atividade_prova_211494/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade_prova_211494
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        int vendas = 1;
+
+        int quantidade;
+
+        double valor_unitario;
+
+        double valor_total;
+
+        double total = 0;
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            dgvProdutos.Rows.Add(txtDescricao.Text, txtQuantidade.Text, $"{double.Parse(txtValorUnitario.Text):C}");
+
+            quantidade = int.Parse(txtQuantidade.Text);
+            valor_unitario = double.Parse(txtValorUnitario.Text);
+            valor_total = quantidade * valor_unitario;
+
+            total += valor_total;
+
+            lblTotal.Text = $"{total:C}";
+
+            vendas++;
+            lblVenda.Text = $"Venda: {vendas}";
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            dgvProdutos.Rows.RemoveAt(dgvProdutos.CurrentCell.ColumnIndex);
+
+        }
+
+        private void dgvProdutos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            dgvProdutos.CurrentRow.Cells[1].Value = txtQuantidadeSelecionada.Text;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpaTextBox(txtDescricao, txtQuantidade, txtQuantidadeSelecionada, txtValorUnitario);
+        }
+
+        private void btnNovaVenda_Click(object sender, EventArgs e)
+        {
+            vendas++;
+            lblVenda.Text = $"Venda: {vendas}";
+            lblItensGrade.Text = $"Itens na grade: {vendas}";
+
+            LimpaTextBox(txtDescricao, txtQuantidade, txtQuantidadeSelecionada, txtValorUnitario);
+        }
+        void LimpaTextBox(params TextBox[] textBoxes)
+        {
+            foreach (TextBox txt in textBoxes)
+            {
+                txt.Clear();
+            }
+        }
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
